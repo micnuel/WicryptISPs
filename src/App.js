92466655459f6e5415ac5d4a7daaa355c3ffec3f
Emmanuel Okoro -267666 <emmanuelokoro@tuni.fi>
2023-01-24 19:52:28 +0100

    ready to push online

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import DataTable from "react-data-table-component";
+
+const columns = [
+  {
+    name: "Device Id",
+    selector: "deviceId",
+    sortable: true,
+  },
+  {
+    name: "ISP",
+    selector: "isp",
+    sortable: true,
+  },
+  {
+    name: "Connection Speed",
+    selector: "connectionSpeed",
+    sortable: true,
+  },
+
+  {
+    name: "Location (City)",
+    selector: "location.city",
+    sortable: true,
+  },
+];
 
 function App() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await fetch(
+          "https://explorer.wicrypt.com/api/v1/Device"
+        );
+        const json = await response.json();
+        console.log(json);
+        setData(json);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    fetchData();
+  }, []);
+
+  const filteredData = data.data.records.filter(
+    (record) =>
+      record.connectionSpeed !== "unknown" && record.connectionSpeed !== "---"
+  );
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {data ? (
+        <DataTable columns={columns} data={filteredData} />
+      ) : (
+        <div>Loading...</div>
+      )}
     </div>
   );
 }
